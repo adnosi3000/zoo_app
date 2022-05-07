ed61b1ec2f4dd2584ab4737717f079e5577ce211
unknown <adamakabeyonce@gmail.com>
2022-05-07 12:25:47 +0200

    changed placement of items, corrected and finished

--- maingui.py
+++ maingui.py
@@ -8,33 +8,33 @@ root.configure(background='grey')
 
 #CONNECTING DATABASE
 connect = Tk.Label(root, text='Connect Your SQL Database')
-connect.place(x=80, y=110)
+connect.place(relx=0.4, rely=0.1, relwidth=0.2)
 connect.configure(fg='black')
 connect.configure(font=("Helvetica", 18))
 connect.configure(background='grey')
 connectinput = Tk.Entry(root, width=55)
 connectinput.insert(0, "name of database")
-connectinput.place(x=80, y=140)
+connectinput.place(relx=0.4, rely=0.15, relwidth=0.2)
 connectbt = Tk.Button(root, text="Connect")
-connectbt.place(x=185,y = 175)
+connectbt.place(relx=0.4, rely=0.2, relwidth=0.2)
 
 #GENERATING REPORT
 report = Tk.Label(root, text='Generate the Report')
-report.place(x=100, y=210)
+report.place(relx=0.4, rely=0.25, relwidth=0.2)
 report.configure(fg='black')
 report.configure(font=("Helvetica", 18))
 report.configure(background='grey')
 menu= Tk.StringVar()
 menu.set("Select Any Table")
 reportselect= Tk.OptionMenu(root, menu, "Workers", "Animals", "Vehicles")
-reportselect.place(x=145, y=265)
+reportselect.place(relx=0.4, rely=0.3, relwidth=0.2)
 reportbt = Tk.Button(root, text="Generate")
-reportbt.place(x=180, y=300)
+reportbt.place(relx=0.4, rely=0.35, relwidth=0.2)
 
 #REGISTER NEW OPTION
 reg = Tk.Label
 reg = Tk.Label(root, text='Register New Item')
-reg.place(x=100, y=300)
+reg.place(relx=0.4, rely=0.4, relwidth=0.2)
 reg.configure(fg='black')
 reg.configure(font=("Helvetica", 18))
 reg.configure(background='grey')
@@ -42,17 +42,20 @@ reg.configure(background='grey')
 menu= Tk.StringVar()
 menu.set("Select Any Option")
 regselect1= Tk.OptionMenu(root, menu, "Workers", "Animals", "Vehicles")
-regselect1.place(x=145, y=345)
+regselect1.place(relx=0.4, rely=0.45, relwidth=0.2)
 
 
 regselect = Tk.Label
 regselect = Tk.Label(root, text='Enter Information')
-regselect.place(x=100, y=385)
+regselect.place(relx=0.4, rely=0.5, relwidth=0.2)
 regselect.configure(fg='black')
 regselect.configure(font=("Helvetica", 18))
 regselect.configure(background='grey')
 regenter = Tk.Entry(root, width=45)
-regenter.place(x=80, y=425)
+regenter.place(relx=0.4, rely=0.55, relwidth=0.2)
+regenterbt = Tk.Button(root, width = 45, text = "Submit Info")
+
+regenterbt.place(relx=0.4, rely=0.6, relwidth =0.2)
 
 
 
